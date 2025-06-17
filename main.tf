94e5ca3a8fb886095f698fb8c7d23c9f6a8f7f8f
Oliver Domokos <domolitom@gmail.com>
2025-06-17 07:45:42 +0200

    add subnet resource

--- main.tf
+++ main.tf
@@ -29,3 +29,10 @@ resource "azurerm_virtual_network" "test-vnet" {
     environment = "dev"
   }
 }
+
+resource "azurerm_subnet" "test-subnet" {
+  name                 = "learning-tf-subnet"
+  resource_group_name  = azurerm_resource_group.test-rg.name
+  virtual_network_name = azurerm_virtual_network.test-vnet.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
