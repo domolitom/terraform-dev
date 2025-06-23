cacafa40b69a837ff05cbde3d9c698318a3c9ff7
Oliver Domokos <domolitom@gmail.com>
2025-06-23 17:36:09 +0200

    correct the subnet

--- main.tf
+++ main.tf
@@ -31,30 +31,8 @@ resource "azurerm_virtual_network" "test-vnet" {
 }
 
 resource "azurerm_subnet" "test-subnet" {
-  name                 = "learning-tf-subnet"
+  name                 = "learning-tf-subnet-1"
   resource_group_name  = azurerm_resource_group.test-rg.name
   virtual_network_name = azurerm_virtual_network.test-vnet.name
   address_prefixes     = ["10.123.1.0/24"]
-}
-
-resource "azurerm_network_security_group" "test-nsg" {
-  name                = "learning-tf-nsg"
-  location            = azurerm_resource_group.test-rg.location
-  resource_group_name = azurerm_resource_group.test-rg.name
-
-  security_rule {
-    name                       = "AllowSSH"
-    priority                   = 1000
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_ranges    = ["22"]
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  tags = {
-    environment = "dev"
-  }
 }
