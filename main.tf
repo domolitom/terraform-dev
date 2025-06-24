eb36b495dd54e95793999c56ceda74d3f4fed899
Oliver Domokos <domolitom@gmail.com>
2025-06-24 20:01:17 +0200

    change platform image for linux vm

--- main.tf
+++ main.tf
@@ -114,7 +114,7 @@ resource "azurerm_linux_virtual_machine" "test-vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "UbuntuServer"
+    offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
